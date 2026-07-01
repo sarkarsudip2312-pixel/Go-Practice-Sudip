c69e59ce15d7acb2af19f3df84a5a9b6199a5073
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-07-01 17:58:49 +0600

    Add files via upload

---
+++ Emni1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+    int b;
+    
+a=13;
+b=7;
+
+    float height=5.11;
+    char grade='A';
+    
+    printf("The Result:\t%d\n", a+b);
+    printf("Name:\t\tSudip Sarkar\n");
+    printf("Deperment:\tCSE\n");
+    printf("Roll:\t\t2520510131\n");
+    printf("Height:\t\t%.1f\n", height);
+    printf("Grede:\t\t%c\n\n", grade);
+    printf("Thank tou so much");
+    
+    return 0;
+}
