a0fdce4bbc64379c3cbbcbf775e1f3fc8babeab5
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-07-05 12:57:32 +0600

    Add files via upload

---
+++ Emni2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+    float a;
+    int b;
+    float c;
+    int d;
+    
+    a=25;
+    b=42;
+    c=3;
+    d=28;
+    
+    printf("Gun:\t%d\n", b*d);
+    printf("Vag:\t%.1f\n", a/c);
+    
+    
+    return 0;
+}
