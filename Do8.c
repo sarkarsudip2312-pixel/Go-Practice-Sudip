f67a0e0786c36769e54dfc55bad0347bba3d81fe
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-07-07 14:48:58 +0600

    Add files via upload

---
+++ Do8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main ()
+{
+    char first;
+    char second;
+    char thard;
+    char fhorth;
+    char fivef;
+    char sixth;
+    
+    first= 'S'; 
+    second= 'A'; 
+    thard= 'R'; 
+    fhorth= 'K'; 
+    fivef= 'A';
+    sixth= 'R';
+    
+    printf("%c%c%c%c%c", first, second, thard, fhorth, fivef, sixth);
+    
+    return 0;
+}
