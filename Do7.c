2eb1eecc9cebdad906a4917d62e026f21cbd44b6
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-07-07 14:49:22 +0600

    Add files via upload

---
+++ Do7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+    char a;
+    char b;
+    char c;
+    char d;
+    char e;
+    
+    a= 'S'; 
+    b= 'U'; 
+    c= 'D'; 
+    d= 'I'; 
+    e= 'P';
+    
+    printf("%c%c%c%c%c", a,b,c,d,e);
+    
+    return 0;
+}
