e17d1a70c4007d64bb0e8a1b65cb0dea9df6be30
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-07-21 01:32:57 +0600

    Add files via upload

---
+++ Do12.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main () {
+    int x= 3;
+    
+    switch(x){
+        case 1:
+        printf ("value is : 1\n");
+        break;
+        
+        case 2:
+        printf ("value is : 2\n");
+        break;
+        
+        case 3:
+        printf ("value is : 3\n");
+        break;
+        
+        default:
+        printf ("value is : unknow\n");
+    }
+    
+    
+    
+    return 0;
+}
