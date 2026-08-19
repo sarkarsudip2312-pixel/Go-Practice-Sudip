92f03bbcc0ae32882dbed6be1669becb6b463c88
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-19 15:00:14 +0600

    Add files via upload

---
+++ Do14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    
+    int a;
+    float x;
+    char c;
+    
+    printf("Enter Your Number: ");
+    scanf("%d", &a);
+    
+    printf("Enter Your GPA: ");
+    scanf("%f", &x);
+    
+}
