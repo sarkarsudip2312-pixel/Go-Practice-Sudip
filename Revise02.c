8833085e21871f2db31ea8635a36865e9633fd1e
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-09-07 13:56:30 +0600

    Add files via upload

---
+++ Revise02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    
+    int a,b,sum;
+    
+    printf("Enter 1st Number: ");
+    scanf("%d", &a);
+    
+    printf("Enter 2nd Number: ");
+    scanf("%d", &b);
+    
+    sum= a+b;
+    
+    printf("Your Result:%d ", sum);
+    return 0;
+}
