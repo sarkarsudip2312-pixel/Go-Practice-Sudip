4462d0bdfba86a27c9f47be418cca887601b9db0
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-09-07 13:56:10 +0600

    Add files via upload

---
+++ Revise01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    int a;
+    float x;
+    
+    a=12;
+    x=4.89;
+    
+    printf("Your Number:%d\n" , a);
+    printf("Your Point:%.2f ", x);
+    
+    
+    return 0;
+}
