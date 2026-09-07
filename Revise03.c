af847ed0c949fe9795e6ff42d882646ede839df2
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-09-07 13:56:44 +0600

    Add files via upload

---
+++ Revise03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+    
+    int a,b;
+    
+    a=20;
+    b=5;
+    
+    printf("Jog:%d\n", a+b);
+    printf("Biyog:%d\n", a-b);
+    printf("Gun:%d\n", a*b);
+    printf("Vag:%d\n",  a/b);
+    
+    return 0;
+}
