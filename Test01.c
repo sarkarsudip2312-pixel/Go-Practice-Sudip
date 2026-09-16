23f7353cc83e68197a5e02719e8b5653754a1c8f
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-09-16 16:53:21 +0600

    Add files via upload

---
+++ Test01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, sum = 0;
+
+    for(i = 1; i <= 100; i++) {
+        sum = sum + i;
+    }
+
+    printf("1 theke 100 porjonto sob sonkhar jogfol = %d\n", sum);
+
+    return 0;
+}
