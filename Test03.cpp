9f4859a8df4d93c7a49ea0655bf5f4815224dc4b
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-09-25 13:35:57 +0600

    Add files via upload

---
+++ Test03.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int a;
+    
+    cin>>a;
+    
+    if(a=8){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    
+    return 0;
+}
